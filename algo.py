ef88f02b4aa51f41e4e98de99e4dcb30d3daf5a8
Dade <dade@live.cn>
2016-09-03 17:24:29 -0400

    add par_checker for simple balance parentheses

--- algo.py
+++ algo.py
@@ -8,3 +8,29 @@ def gcd(m,n):
         m = old_n
         n = old_m % old_n
     return n
+
+# parentheses checker for: [,{,(,),},]
+from stru import Stack
+def par_checker(symbol_string):
+    s = Stack()
+    balanced = True
+    index = 0
+    while index < len(symbol_string) and balanced:
+        symbol = symbol_string[index]
+        if symbol in "({[":
+            s.push(symbol)
+        else:
+            if s.is_empty():
+                balanced = False
+            else:
+                top = s.pop()
+                if not matches(top, symbol):
+                    balanced = False
+        index = index + 1
+
+    return balanced and s.is_empty()
+
+def matches(open, close):
+    opens = "([{"
+    closes = ")]}"
+    return opens.index(open) == closes.index(close)
