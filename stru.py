74e6de9d63c989cdb7803412e71e2139cfaeeb48
Dade <dade@live.cn>
2016-09-06 15:50:28 -0400

    add Node

--- stru.py
+++ stru.py
@@ -68,3 +68,21 @@ class Deque:
 
     def __str__(self):
         return str(self.items)
+
+# Implementation of a Node
+class Node:
+    def __init__(self, init_data):
+        self.data = init_data
+        self.next = None
+
+    def get_data(self):
+        return self.data
+
+    def get_next(self):
+        return self.next
+
+    def set_data(self, new_data):
+        self.data = new_data
+
+    def set_next(self, new_next):
+        self.next = new_next
