78d19e244fa1cffd7eca2d4921445e54fbadd1f7
Dade <dade@live.cn>
2016-09-06 14:53:44 -0400

    add base_converter

--- algo.py
+++ algo.py
@@ -1,5 +1,7 @@
 # module for common python algorithms
 
+from stru import Stack
+
 # greatest common divisor
 def gcd(m,n):
     while m % n != 0:
@@ -10,7 +12,6 @@ def gcd(m,n):
     return n
 
 # parentheses checker for: [,{,(,),},]
-from stru import Stack
 def par_checker(symbol_string):
     s = Stack()
     balanced = True
@@ -34,3 +35,19 @@ def matches(open, close):
     opens = "([{"
     closes = ")]}"
     return opens.index(open) == closes.index(close)
+
+# decimal numbers to base numbers
+def base_converter(dec_number, base):
+    digits = "0123456789ABCDEF"
+    rem_stack = Stack()
+
+    while dec_number > 0:
+        rem = dec_number % base
+        rem_stack.push(rem)
+        dec_number = dec_number // base
+
+    new_string = ""
+    while not rem_stack.is_empty():
+        new_string = new_string + digits[rem_stack.pop()]
+
+    return new_string
