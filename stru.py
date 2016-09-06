7c372c4aeebfd654d52b0c3623083eddd8834d43
Dade <dade@live.cn>
2016-09-06 15:14:56 -0400

    add Queue

--- stru.py
+++ stru.py
@@ -22,3 +22,23 @@ class Stack:
 
     def __str__(self):
         return str(self.items)
+
+# Implementation of a Queue
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        self.items.insert(0, item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+    def __str__(self):
+        return str(self.items)
