a3874ce92634a5ea094f249c51f08b6e2a20d8ed
Dade <dade@live.cn>
2016-09-08 22:26:16 -0400

    add sequential and binary search

--- algo.py
+++ algo.py
@@ -77,3 +77,64 @@ def pal_checker(a_string):
             still_equal = False
 
     return still_equal
+
+# sequential search
+def sequential_search(a_list, item):
+    pos = 0
+    found = False
+
+    while pos < len(a_list) and not found:
+        if a_list[pos] == item:
+            found = True
+        else:
+            pos = pos + 1
+
+    return found
+
+# ordered sequential search
+def ordered_sequential_search(a_list, item):
+    pos = 0
+    found = False
+    stop = False
+    while pos < len(a_list) and not found and not stop:
+        if a_list[pos] == item:
+            found = True
+        else:
+            if a_list[pos] > item:
+                stop = True
+            else:
+                pos = pos + 1
+
+    return found
+
+# binary search
+def binary_search(a_list, item):
+    first = 0
+    last = len(a_list) - 1
+    found = False
+
+    while first <= last and not found:
+        midpoint = (first + last) // 2
+        if a_list[midpoint] == item:
+            found = True
+        else:
+            if item < a_list[midpoint]:
+                last = midpoint - 1
+            else:
+                first = midpoint + 1
+
+    return found
+
+# binary search using recursion
+def binary_search_recur(a_list, item):
+    if len(a_list) == 0:
+        return False
+    else:
+        midpoint = len(a_list) // 2
+        if a_list[midpoint] == item:
+            return True
+        else:
+            if item < a_list[midpoint]:
+                return binary_search_recur(a_list[:midpoint], item)
+            else:
+                return binary_search_recur(a_list[midpoint + 1:], item)
