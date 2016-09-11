d43be032f1f9495f91b3092924bcdcaf12f17bca
Dade <dade@live.cn>
2016-09-11 16:40:56 -0400

    add insertion sort

--- algo.py
+++ algo.py
@@ -177,3 +177,14 @@ def selection_sort(a_list):
 
         a_list[fill_slot], a_list[pos_of_max] = \
         a_list[pos_of_max], a_list[fill_slot]
+
+# insertion sort (n*n)
+def insertion_sort(a_list):
+    for index in range(1, len(a_list)):
+        current_value = a_list[index]
+        position = index
+        while position > 0 and a_list[position-1] > current_value:
+            a_list[position] = a_list[position-1]
+            position = position - 1
+
+        a_list[position] = current_value
