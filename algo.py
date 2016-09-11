298b35a0e728caeb2f327650b9f9f4db4e236709
Dade <dade@live.cn>
2016-09-11 16:10:29 -0400

    add bubble sort

--- algo.py
+++ algo.py
@@ -146,3 +146,23 @@ def hash_string(a_string, table_size):
         sum = sum + ord(a_string[pos])
 
     return sum % table_size
+
+# bubble sort (n*n)
+def bubble_sort(a_list):
+    for pass_num in range(len(a_list) - 1, 0, -1):
+        for i in range(pass_num):
+            if a_list[i] > a_list[i+1]:
+                a_list[i], a_list[i+1] = a_list[i+1], a_list[i]
+
+# short bubble sort (n*n)
+def short_bubble_sort(a_list):
+    exchanges = True
+    pass_num = len(a_list) - 1
+    while pass_num > 0 and exchanges:
+        exchanges = False
+        for i in range(pass_num):
+            if a_list[i] > a_list[i+1]:
+                exchanges = True
+                a_list[i], a_list[i+1] = a_list[i+1], a_list[i]
+
+        pass_num = pass_num - 1
